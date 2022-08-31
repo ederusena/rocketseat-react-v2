d02470d13d4904fcc5c76843f6c3cbac067508ba
Eder Sena <eder.sena@live.com>
2022-08-31 17:48:52 -0300

    post done

--- aulas/01-fundamentos-react/src/components/Post/index.jsx
+++ aulas/01-fundamentos-react/src/components/Post/index.jsx
@@ -1,10 +1,40 @@
+import styles from './index.module.css';
+import profileImg from '../../assets/ederperfil.jpg';
+
 export function Post(props) {
-  const { author, content } = props
+  const { author, content } = props;
   return (
-    <div>
-      <h2>{author}</h2>
-      <h4>{content}</h4>
-      <button className="btn">Hello daisyUI</button>
-    </div>
-  )
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src={profileImg} />
+          <div className={styles.authorInfo}>
+            <strong>Eder Sena</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title='31 de agosto às 14:24' dateTime='2022-08-31 14:23:00'>
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href=''>jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href=''>#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
 }
--- aulas/01-fundamentos-react/src/components/Sidebar/index.jsx
+++ aulas/01-fundamentos-react/src/components/Sidebar/index.jsx
@@ -1,16 +1,24 @@
 import styles from "./index.module.css";
+import capaImg from "../../assets/capa.avif";
+import profileImg from "../../assets/ederperfil.jpg";
+import { PencilLine } from "phosphor-react";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+      <img className={styles.cover} src={capaImg} />
+
       <div className={styles.profile}>
+        <img src={profileImg} />
         <strong>Eder Soares</strong>
         <span>Frontend Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
